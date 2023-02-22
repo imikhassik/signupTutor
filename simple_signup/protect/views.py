8f48105e326905a8c9144ef5ce7e3ed19f7211c4
Ilya Mikhasik <imikhassik@gmail.com>
2023-02-22 03:17:41 -0500

    Update db, fix protect/views and sign/login.html

--- simple_signup/protect/views.py
+++ simple_signup/protect/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class IndexView(LoginRequiredMixin, TemplateView):
-    template_name = 'protect/index.tml'
+    template_name = 'protect/index.html'
