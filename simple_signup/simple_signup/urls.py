d8c791b8c894c3defad57c030336382cc33fd1e4
Ilya Mikhasik <imikhassik@gmail.com>
2023-02-22 04:12:03 -0500

    Add accounts/base.html

--- simple_signup/simple_signup/urls.py
+++ simple_signup/simple_signup/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('protect.urls')),
     path('sign/', include('sign.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
